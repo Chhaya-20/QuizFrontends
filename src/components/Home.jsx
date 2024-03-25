2fa9749ca42ba7c8a61de6f026ecfe930941f1ea
Chhaya-20 <157710352+Chhaya-20@users.noreply.github.com>
2024-03-25 13:26:24 +0530

    Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -66,7 +66,7 @@ function Home() {
          {loading ? (
       <div
         
-        style={{ height: "100vh" , "width":"100vw"  , "display":"flex" , "justifyContent":"center" ,  "alignItems" : "center"}}
+        style={{ height: "100vh" , "width":"100vw"  , "display":"flex" , "justifyContent":"center" ,  "alignItems" : "center","flexDirection":"column"}}
       >
         <img
           style={{ height: "10vh" }}
