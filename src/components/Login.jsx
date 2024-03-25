b115125c16daa7431dc33f2d55db0afed05d3ac9
Chhaya-20 <157710352+Chhaya-20@users.noreply.github.com>
2024-03-25 13:26:42 +0530

    Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -44,7 +44,7 @@ function Login() {
     {loading ? (
       <div
         className="flex flex-col justify-center items-center m-0 p-0"
-        style={{ height: "100vh" , "width":"100vw"  , "display":"flex" , "justifyContent":"center" ,  "alignItems" : "center"}}
+        style={{ height: "100vh" , "width":"100vw"  , "display":"flex" , "justifyContent":"center" ,  "alignItems" : "center","flexDirection":"column"}}
       >
         <img
           style={{ height: "10vh" }}
